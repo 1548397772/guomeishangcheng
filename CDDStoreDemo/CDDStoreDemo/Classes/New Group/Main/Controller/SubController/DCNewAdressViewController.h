9c3448a7d8ee65d7d6a3c04bb8e24b48057de6cc
1548397772 <xsb537@163.com>
2018-02-11 14:14:29 +0800

    init

---
+++ CDDStoreDemo/CDDStoreDemo/Classes/New Group/Main/Controller/SubController/DCNewAdressViewController.h
@@ -0,0 +1,26 @@
+//
+//  DCNewAdressViewController.h
+//  CDDStoreDemo
+//
+//  Created by 陈甸甸 on 2017/12/19.
+//Copyright © 2017年 RocketsChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    DCSaveAdressNewType = 0,
+    DCSaveAdressChangeType = 1,
+} DCSaveAdressType;
+
+@class DCAdressItem;
+@interface DCNewAdressViewController : UIViewController
+
+
+/* type */
+@property (nonatomic,assign) DCSaveAdressType saveType;
+
+/* 更改数据 */
+@property (strong , nonatomic)DCAdressItem *adressItem;
+
+@end
